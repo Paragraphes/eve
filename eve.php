6a17a553d5fce25406a1d7f5064e73caf21475e2
paranoodle <paranoodle@users.noreply.github.com>
2015-09-10 14:36:32 +0200

    switching back to original settings

--- eve.php
+++ eve.php
@@ -7,11 +7,11 @@ date_default_timezone_set('Europe/Zurich');
 define("EVE_APP", true);
 
 require 'Library/autoload.php';
-//require 'Library/errorHandler.php';
+require 'Library/errorHandler.php';
 
 try {
-	//$app = new Applications\Eve\EveApplication(__DIR__);
-	$app = new Applications\Eve\EveApplication('/var/www/html/eve');
+	$app = new Applications\Eve\EveApplication(__DIR__);
+	
 	$app->run();
 } catch (Exception $e) {
 	echo "An error has occured, message was:<br>" . $e->getMessage();
